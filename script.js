ae64ef932e322606fee3a89883b1ae336a7a3bdc
kevinle98 <kevin.le@ucf.edu>
2023-11-22 07:51:13 -0500

    Added 2 classes

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let paladinImg, rougeImg, wizardImg;
-let selectedCharacter = '';
+let paladinImg, rougeImg, wizardImg, druidImg, barbarianImg;
+let viewedCharacter = '';
+let selectedCharacter;
 let characters;
-let portraitSize = { width: 260, height: 430 };
+let portraitSize = { width: 200, height: 320 };
 
 function preload() {
+    barbarianImg = loadImage('Barbarian.png');
     paladinImg = loadImage('Paladin.png');
     rougeImg = loadImage('Rouge.png');
+    druidImg = loadImage('Druid.png');
     wizardImg = loadImage('Wizard.png');
 }
 
@@ -16,60 +19,79 @@ function setup() {
     textFont('monospace');
 
     characters = {
-        'Paladin': {
-            img: paladinImg,
-            tech: 80,
-            combat: 30,
-            magic: 10,
-            x: 300,
+        'Barbarian': {
+            img: barbarianImg,
+            strength: 15,
+            intelligence: 8,
+            finesse: 13,
+            x: 150,
             y: 450
         },
-        'Rouge': {
+        'Paladin': {
+            img: paladinImg,
+            strength: 14,
+            intelligence: 12,
+            finesse: 10,
+            x: 375,
+            y: 450
+        },
+        'Rogue': {
             img: rougeImg,
-            tech: 10,
-            combat: 80,
-            magic: 30,
+            strength: 10,
+            intelligence: 11,
+            finesse: 15,
             x: 600,
+            y: 450
+        },
+        'Druid': {
+            img: druidImg,
+            strength: 12,
+            intelligence: 14,
+            finesse: 10,
+            x: 825,
             y: 450
         },
         'Wizard': {
             img: wizardImg,
-            tech: 10,
-            combat: 30,
-            magic: 80,
-            x: 900,
+            strength: 8,
+            intelligence: 15,
+            finesse: 13,
+            x: 1050,
             y: 450
-        }
+        },
     };
+    
+    
 }
 
 function draw() {
     background(15);
 
-    if (selectedCharacter && characters[selectedCharacter]) {
-        let character = characters[selectedCharacter];
+    if (viewedCharacter && characters[viewedCharacter]) {
+        let character = characters[viewedCharacter];
         let imgScale = 0.8
 
         tint(255, 255, 255);
-        image(character.img, width / 2, height / 2, character.img.width * imgScale, character.img.height * imgScale);
+        image(character.img, width / 2, (height / 2) - 70, character.img.width * imgScale, character.img.height * imgScale);
 
         fill(255);
         textSize(40);
-        text(selectedCharacter, width / 2, height / 6);
+        text(viewedCharacter, width / 2, height / 10);
 
-        textSize(24);
-        text('Tech: ' + character.tech, width / 2, height - 150);
-        text('Combat: ' + character.combat, width / 2, height - 120);
-        text('Magic: ' + character.magic, width / 2, height - 90);
+        textSize(22);
+        text('Strength: ' + character.strength, width / 2, height - 240);
+        text('Intelligence: ' + character.intelligence, width / 2, height - 210);
+        text('Finesse: ' + character.finesse, width / 2, height - 180);
 
-        textSize(14);
-        text('Press "R" to reset character selection', width / 2, height - 40);
+        textSize(18);
+        text('Press "Esc" to go back to character selection', width / 2, height - 50);
+        text('Press "Space" confirm character', width / 2, height - 75);
     } else {
 
         for (let characterName in characters) {
             let character = characters[characterName];
             let isHovered = isMouseOverPortrait(character);
-            tint(isHovered ? 255 : 100);
+            tint(isHovered ? 255 : 80);
             image(character.img, character.x, character.y, portraitSize.width, portraitSize.height);
         }
     }
@@ -83,11 +105,11 @@ function isMouseOverPortrait(character) {
 }
 
 function mousePressed() {
-    if (!selectedCharacter) {
+    if (!viewedCharacter) {
         for (let characterName in characters) {
             let character = characters[characterName];
             if (isMouseOverPortrait(character)) {
-                selectedCharacter = characterName;
+                viewedCharacter = characterName;
                 break;
             }
         }
@@ -95,11 +117,12 @@ function mousePressed() {
 }
 
 function resetSketch() {
-    selectedCharacter = '';
+    viewedCharacter = '';
 }
-
 function keyPressed() {
-    if (key === 'r' || key === 'R') {
+    if (keyCode === ESCAPE) {
         resetSketch();
+    } else if (keyCode === 32) {
+        selectedCharacter = viewedCharacter;
     }
 }
